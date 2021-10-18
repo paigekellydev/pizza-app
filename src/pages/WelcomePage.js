db873c8c97711e60ade33558495b606deb6ef5ee
Paige <72361304+pckellyco@users.noreply.github.com>
2021-10-18 11:39:01 -0600

    Added buttons to welcome page

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function WelcomePage() {
     return (
         <div id="welcome-page" className="page">
-            <h2>Welome</h2>
+            <h2>Welcome</h2>
+            <button className="welcome-page-button">
+                <Link exact to="/order">Order Online</Link>
+            </button>
+            <button className="welcome-page-button">
+                <Link exact to="/contact">Contact</Link>
+            </button>
         </div>
     )
 }
