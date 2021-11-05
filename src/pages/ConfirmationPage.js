e37ed422ae04f9e7b866452d2c372382b92fdbce
Paige <72361304+pckellyco@users.noreply.github.com>
2021-11-05 13:39:33 -0600

    form submit is working

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -5,15 +5,26 @@ import { store } from "../store"
 export default function ConfirmationPage() {
     const store = useSelector(store => store)
     const url = `http://localhost:3000/pizzaOrders`
-    const orderInfo = localStorage.getItem('orderInfo')
+    const orderInfo = JSON.parse(localStorage.getItem('orderInfo'))
     const display = () => console.log(orderInfo)
+    const deliveryMessage = () => {
+        if (orderInfo.delivery.type === "pickUp")  {
+            return (
+                <section id="pickUpInfo"> 
+                    <p>Your order will be available in 20 minutes. 
+                        Please pick up at 1000 Alaska Drive,
+                    </p>
+                </section>
+            )
+        }
+    }
 
     return (
         <div>
-            <h2>Thank you for placing your order</h2>
+            <h2>Thank you {orderInfo.contactInfo.firstName} for placing your order</h2>
             <h4>Contact Information: </h4>
             {display()}
-            <p>Name: {store.contactInfo.firstName} {store.contactInfo.lastName}</p>
+            <p>Name:  {store.contactInfo.lastName}</p>
             <h4>Delivery Information: </h4>
             <h4>Pizza Information</h4>
         </div>
